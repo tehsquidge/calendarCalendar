343853dd02ecba6716e21ac671b5b7e750d8db75
Liam Kerr <liam.d.kerr@gmail.com>
2016-11-03 14:46:05 +0000

    Adding Arabic and Turkish lexicons

---
+++ lexicons/tr.js
@@ -0,0 +1,9 @@
+var lexicon = { 
+	titleDays: ['Su','Mo','Tu','We','Th','Fr','Sa'],
+	shortDays: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+	longDays: ['Pazar','Pazartesi','Salı','Çarşamba','Perşembe','Cuma','Cumartesi'],
+	shortMonths: ['Oca','Şub','Mar','Nis','May','Haz','Tem','Ağu','Eyl','Eki','Kas','Ara'],
+	longMonths: ['Ocak','Şubat','Mart','Nisan','Mayıs','Haziran','Temmuz','Ağustos','Eylül','Ekim','Kasım','Aralık'],
+	startCalendarTitle: 'Giriş',
+	endCalendarTitle: 'Çıkış'
+}
